ac2627e0801985d53924d5fdaa9c91dd69a223ab
Marius Mandal <mariusmandal@gmail.com>
2015-04-16 10:54:48 +0200

    Forskjellige bandwidth-modes for UKM-TV

--- controller/home.controller.php
+++ controller/home.controller.php
@@ -1,3 +1,7 @@
 <?php
 require_once('class/home.controller.php');
 $controller = new controller_home();
+
+if( isset($_GET['bandwidth'] ) ) {
+	$controller->setBandwidthMode( $_GET['bandwidth'] );
+}
