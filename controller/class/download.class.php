52458114fc028aafccf7624f5e55dc6a1d24b417
Asgeir Stavik Hustad <askhus@online.no>
2016-02-10 13:30:41 +0000

    Disable normal kvalitet-knapp om vi kun har 720p-fil.

--- controller/class/download.class.php
+++ controller/class/download.class.php
@@ -31,6 +31,11 @@ class download_controller extends UKMController {
 		
 		$tv->videofile();
 		#var_dump($tv);
+
+		// Sjekk om vi kun har 720p
+        if(substr($tv->file, -8, 4) == '720p') {
+            $this->data['720ponly'] = true;
+        }
 		$this->data['video'] = $tv;
 		$this->data['video_url'] = $tv->storageurl . $tv->file;
 		$this->data['file_path'] = $this->findFilePath($tv->cron_id);
